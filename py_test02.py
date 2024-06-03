e2b945018b7238e13fa496751b8302241dcbb405
Jongsung Bang <tjbh@naver.com>
2024-06-03 14:43:11 +0900

    First Commit

---
+++ py_test02.py
@@ -0,0 +1,16 @@
+# Swap Variables in Python without a Temporary Variable
+
+x = 3
+y = 6
+
+print('x swap befor : ', x)
+print('y swap befor : ', y)
+
+x, y = y, x
+
+print('\nx y Swap\n')
+
+print('x equals : ', x)
+print('y equals : ', y)
+
+# End of file
